e799029ff960e1e2838f78d8cde6be812467451d
Mihai Dinca <dincamihai@gmail.com>
2014-07-26 19:12:52 +0300

    refactor tests

--- standard/schema/tests/test_validate_schema.py
+++ standard/schema/tests/test_validate_schema.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 import json
+from path import path
 from jsonschema import validate, ValidationError
 
-from validate_schema import validate_release_schema
+
+DRAFT_SCHEMA_PATH = path(__file__).parent / 'json_schema/json-schema-draft-4.json'
+RELEASE_SCHEMA_PATH = path(__file__).parent.parent / 'release-schema.json'
+
+
+def validate_release_schema():
+    with open(DRAFT_SCHEMA_PATH, 'r') as f:
+        draft_schema = json.loads(f.read())
+    with open(RELEASE_SCHEMA_PATH, 'r') as f:
+        release_schema = json.loads(f.read())
+    validate(release_schema, draft_schema)
+    print "SUCCESS: Schema is valid against JSON Schema Draft 4."
 
 
 def test_validate_release_schema():
