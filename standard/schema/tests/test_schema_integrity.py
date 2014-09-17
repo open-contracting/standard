99788044058ef178eb1f21ea9289bc8de096eacf
Sarah Bird <sarah@bonvaya.com>
2014-09-16 23:00:59 -0700

    Add tests for schema integrity
    The versioned release schema should be in sync with the release schema
    and the versioned validation schema should by up to date too.

---
+++ standard/schema/tests/test_schema_integrity.py
@@ -0,0 +1,46 @@
+import json
+import jsonmerge
+
+from .test_validate_schema import (
+    RELEASE_SCHEMA_PATH,
+    VERSIONED_RELEASE_SCHEMA_PATH,
+    VERSIONED_RELEASE_VALIDATION_SCHEMA_PATH
+)
+
+from ..utils.make_validation_schema import get_versioned_validation_schema
+
+
+def test_versioned_release_validation_schema_is_in_sync():
+    # Builds the validation schema and confirms its equal to the stored schema
+    with open(VERSIONED_RELEASE_VALIDATION_SCHEMA_PATH, "rb") as f:
+        actual = json.loads(f.read())
+
+    with open(VERSIONED_RELEASE_SCHEMA_PATH, "rb") as f:
+        versioned_release = json.loads(f.read())
+
+    expected = get_versioned_validation_schema(versioned_release)
+
+    assert expected == actual
+
+
+def test_versioned_release_schema_and_release_schema_types_are_in_sync():
+    # Versioned release schema has additional merge strategies and small
+    # tweaks, but should be the same as release schema, so need to confirm.
+
+    with open(RELEASE_SCHEMA_PATH, "rb") as f:
+        release = json.loads(f.read())
+
+    with open(VERSIONED_RELEASE_SCHEMA_PATH, "rb") as f:
+        versioned_release = json.loads(f.read())
+
+    def recurse(original, should_be_equal, k):
+        keywords_to_ignore = ["title", "description", "mergeStrategy", "$ref"]
+        if isinstance(original, dict):
+            for k in original:
+                if k not in keywords_to_ignore:
+                    recurse(original[k], should_be_equal[k], k)
+        else:
+            if k == 'type':
+                assert original == should_be_equal
+
+    recurse(release, versioned_release, None)
---
+++ standard/schema/utils/make_validation_schema.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import jsonmerge
+import json
+
+
+def get_versioned_validation_schema(versioned_release):
+    merger = jsonmerge.Merger(versioned_release)
+
+    versioned_validation_schema = merger.get_schema()
+    versioned_validation_schema["id"] = "https://raw.githubusercontent.com/open-contracting/standard/master/standard/schema/versioned-release-validation-schema.json"  # nopep8
+    versioned_validation_schema["$schema"] = "http://json-schema.org/draft-04/schema#"  # nopep8
+    versioned_validation_schema["title"] = "Schema for a compiled, versioned Open Contracting Release."  # nopep8
+
+    return versioned_validation_schema
+
+
+if __name__ == "__main__":
+    from os.path import abspath, dirname, join
+
+    schema_dir = dirname(dirname(abspath(__file__)))
+
+    with open(join(schema_dir, 'versioned-release-schema.json'), 'rb') as f:
+        vr = json.loads(f.read())
+
+    new_validation_schema = get_versioned_validation_schema(vr)
+
+    with open(join(schema_dir, 'versioned-release-validation-schema.json'), 'wb') as f:
+        f.write(json.dumps(new_validation_schema, indent=4))
