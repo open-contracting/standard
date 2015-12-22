14178216f295aea61af5f69604443ff03c63212d
Ben Webb <ben.webb@opendataservices.coop>
2015-12-22 11:54:30 +0000

    Copy assets folder into the build directory

--- build_docs.sh
+++ build_docs.sh
@@ -2,3 +2,5 @@ mkdir standard/docs/field_definitions
 python standard/schema/utils/make_field_definitions.py
 cd standard && sphinx-build -b dirhtml docs/en ../build/en
 # sphinx-build -b dirhtml -D language='es' standard/docs/en build/es
+cd ..
+cp -r standard/assets build
