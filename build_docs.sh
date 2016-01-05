cf43eddd407f870456bb9923fc534378761e645b
Ben Webb <ben.webb@opendataservices.coop>
2016-01-05 11:12:10 +0000

    Add set -e to bash script

--- build_docs.sh
+++ build_docs.sh
@@ -1,4 +1,5 @@
-mkdir standard/docs/field_definitions
+set -e
+mkdir -p standard/docs/field_definitions
 python standard/schema/utils/make_field_definitions.py
 cd standard && sphinx-build -b dirhtml docs/en ../build/en
 # sphinx-build -b dirhtml -D language='es' standard/docs/en build/es
