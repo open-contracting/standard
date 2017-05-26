647184caa0ca293b94dec0dc191781974659272f
Tim Davies <tim.davies@opendataservices.coop>
2017-05-26 10:59:23 +0200

    Updated schema links and translations

--- standard/schema/utils/make_validation_schema.py
+++ standard/schema/utils/make_validation_schema.py
@@ -91,7 +91,7 @@ def unversion_refs(schema):
 
 
 def get_versioned_validation_schema(versioned_release):
-    versioned_release["id"] = "http://standard.open-contracting.org/schema/1__0__2/versioned-release-validation-schema.json"  # nopep8
+    versioned_release["id"] = "http://standard.open-contracting.org/schema/1__1__0/versioned-release-validation-schema.json"  # nopep8
     versioned_release["$schema"] = "http://json-schema.org/draft-04/schema#"  # nopep8
     versioned_release["title"] = "Schema for a compiled, versioned Open Contracting Release."  # nopep8
 
