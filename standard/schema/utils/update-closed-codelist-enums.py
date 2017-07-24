e52d00f96725ed75c75c3f6c5477f4625b5ba000
Tim Davies <tim.davies@opendataservices.coop>
2017-07-24 15:39:03 +0100

    Updated validation schema and codelist script formatting

--- standard/schema/utils/update-closed-codelist-enums.py
+++ standard/schema/utils/update-closed-codelist-enums.py
@@ -10,16 +10,16 @@ from collections import OrderedDict
 from os.path import abspath, dirname, join
 
 
-
-def get_enum(codelist,includeNull = False):
+def get_enum(codelist, includeNull=False):
     codes = []
-    with open(join(schema_dir, 'codelists',codelist), 'r') as csvfile:
+    with open(join(schema_dir, 'codelists', codelist), 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             codes.append(row['Code'])
     if includeNull:
         codes.append(None)
     return codes
+
 
 schema_dir = dirname(dirname(abspath(__file__)))
 
@@ -28,13 +28,14 @@ with open(join(schema_dir, 'release-schema.json'), 'r') as f:
 
 release_schema['properties']['tag']['items']['enum'] = get_enum("releaseTag.csv")
 release_schema['properties']['initiationType']['enum'] = get_enum("initiationType.csv")
-release_schema['definitions']['Tender']['properties']['status']['enum'] = get_enum("tenderStatus.csv",True)
-release_schema['definitions']['Tender']['properties']['procurementMethod']['enum'] = get_enum("method.csv",True)
-release_schema['definitions']['Tender']['properties']['mainProcurementCategory']['enum'] = get_enum("procurementCategory.csv",True)
-release_schema['definitions']['Award']['properties']['status']['enum'] = get_enum("awardStatus.csv",True)
-release_schema['definitions']['Contract']['properties']['status']['enum'] = get_enum("contractStatus.csv",True)
-release_schema['definitions']['Milestone']['properties']['status']['enum'] = get_enum("milestoneStatus.csv",True)
-release_schema['definitions']['Value']['properties']['currency']['enum'] = get_enum("currency.csv",True)
+release_schema['definitions']['Tender']['properties']['status']['enum'] = get_enum("tenderStatus.csv", True)
+release_schema['definitions']['Tender']['properties']['procurementMethod']['enum'] = get_enum("method.csv", True)
+release_schema['definitions']['Tender']['properties']['mainProcurementCategory']['enum'] = get_enum(
+    "procurementCategory.csv", True)
+release_schema['definitions']['Award']['properties']['status']['enum'] = get_enum("awardStatus.csv", True)
+release_schema['definitions']['Contract']['properties']['status']['enum'] = get_enum("contractStatus.csv", True)
+release_schema['definitions']['Milestone']['properties']['status']['enum'] = get_enum("milestoneStatus.csv", True)
+release_schema['definitions']['Value']['properties']['currency']['enum'] = get_enum("currency.csv", True)
 
 with open(join(schema_dir, 'release-schema.json'), 'w+') as f:
     json.dump(release_schema, f, indent=2, separators=(',', ': '))
