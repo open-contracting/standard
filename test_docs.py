3653234fa2fd0966e014cf58c605a5d661390aa4
Ben Webb <ben.webb@opendataservices.coop>
2017-08-24 17:30:17 +0100

    [#544] Add a test of the language switcher
    Can't run against localhost (is skipped).
    Must be run with:
    CUSTOM_SERVER_URL='http://standard.open-contracting.org/1.1/' py.test test_docs.py

--- test_docs.py
+++ test_docs.py
@@ -7,6 +7,7 @@ from multiprocessing import Process
 from collections import OrderedDict
 import time
 import re
+from selenium.webdriver.support.ui import Select
 
 
 BROWSER = os.environ.get('BROWSER', 'ChromeHeadless')
@@ -27,6 +28,9 @@ def browser(request):
 
 @pytest.fixture(scope="module")
 def server(request):
+    if 'CUSTOM_SERVER_URL' in os.environ:
+        return os.environ['CUSTOM_SERVER_URL']
+
     host = 'localhost'
     port_number = 8331
 
@@ -46,11 +50,14 @@ def server(request):
     return 'http://{}:{}/'.format(host, port_number)
 
 
-@pytest.mark.parametrize('lang,text', [
+lang_basic_text = OrderedDict([
     ('en', 'Open Contracting Data Standard'),
     ('es', 'Estándar de Datos de Contrataciones Abiertas'),
     ('fr', 'Standard de Données sur la Commande Publique Ouverte'),
 ])
+
+
+@pytest.mark.parametrize('lang,text', lang_basic_text.items())
 def test_basic(browser, server, lang, text):
     browser.get('{}{}'.format(server, lang))
     assert text in browser.find_element_by_tag_name('body').text
@@ -65,5 +72,22 @@ def test_search(browser, server, lang, regex):
     browser.get('{}{}'.format(server, lang))
     search_box = browser.find_element_by_id('rtd-search-form').find_element_by_tag_name('input')
     search_box.send_keys('tender\n')
-    time.sleep(1)
+    time.sleep(2)
     assert re.search(regex, browser.find_element_by_tag_name('body').text)
+
+
+def test_language_switcher(browser, server):
+    if 'localhost' in server:
+        pytest.skip()
+
+    browser.get('{}en'.format(server))
+
+    for lang, lang_name in [
+            ('es', 'Español'),
+            ('fr', 'Français'),
+            ('en', 'English'),
+            ]:
+        select = Select(browser.find_element_by_xpath("//select[@name='lang']"))
+        select.select_by_visible_text(lang_name)
+        assert browser.current_url == '{}{}/'.format(server, lang)
+        assert lang_basic_text[lang] in browser.find_element_by_tag_name('body').text
