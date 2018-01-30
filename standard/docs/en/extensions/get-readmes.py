76bd4731396dbc1a40046be758e5797778dd69c5
James McKinney <james@slashpoundbang.com>
2018-01-29 20:23:02 -0500

    get-readmes.py: Add a means to pull from different refs for the registry and extensions

--- standard/docs/en/extensions/get-readmes.py
+++ standard/docs/en/extensions/get-readmes.py
@@ -7,8 +7,9 @@ Download extension README.md files and add them to this directory.
 Add an extra Metadata section to the top of each file.
 '''
 
-GIT_REF = "v1.1.3"
-location = "http://standard.open-contracting.org/extension_registry/{}/extensions.json".format(GIT_REF)
+EXTENSION_GIT_REF = "v1.1.3"
+REGISTRY_GIT_REF = "master"
+location = "http://standard.open-contracting.org/extension_registry/{}/extensions.json".format(REGISTRY_GIT_REF)
 extension_json = requests.get(location).json()
 
 metadata = '''
@@ -41,5 +42,8 @@ for extension in extension_json['extensions']:
             heading = '\n'.join(lines[:header_rows])
             body = '\n'.join(lines[header_rows:])
             body = body.replace('\n##', '\n###')
-            text = heading + metadata.format(extension['url'], github_repo_url, github_repo_url) + body
+            url = extension['url']
+            if REGISTRY_GIT_REF != EXTENSION_GIT_REF:
+                url = url.replace(REGISTRY_GIT_REF, EXTENSION_GIT_REF)
+            text = heading + metadata.format(url, github_repo_url, github_repo_url) + body
             readme.write(text)
