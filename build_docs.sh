8aa67fbb7aac0fbc83379cceceb0b60ac57de57b
James McKinney <james@slashpoundbang.com>
2018-01-31 18:25:18 -0500

    Use command-line argument instead of environment variable, add comments in build_docs.sh

--- build_docs.sh
+++ build_docs.sh
@@ -1,41 +1,55 @@
 #!/bin/bash
 set -e
-cd standard
-CODELIST_LANG=en python schema/utils/translate_codelists.py schema
-CODELIST_LANG=en python schema/utils/translate_codelists.py docs/en/extensions
 
+cd standard
+
+python schema/utils/translate_codelists.py schema en
+python schema/utils/translate_codelists.py docs/en/extensions en
+
+# Compile message catalogs in the "schema" and "codelists" domains to MO files.
 pybabel compile --use-fuzzy -d docs/locale -D schema
 pybabel compile --use-fuzzy -d docs/locale -D codelists
 
 cd ..
+
+# Add translated JSON Schema files to the build directory.
 python standard/schema/utils/translate_schema.py en es fr
 
-# Create a symlink for the current language, so we can reference the
-# translated JSON schema from Sphinx directives
-rm -f build/current_lang || true
+# Create a symlink for the current language, so we can reference the source JSON Schema from Sphinx directives.
+rm -f build/current_lang
 ln -s en build/current_lang
 
 cd standard
+
+# Build the English documentation and message catalogs.
 sphinx-build -q -b dirhtml docs/en ../build/en
 sphinx-build -q -b gettext docs/en ../build/locale
 
+# Use the Babel extractors.
 pybabel -q extract -F .babel_schema . -o ../build/locale/schema.pot
 pybabel -q extract -F .babel_codelists . -o ../build/locale/codelists.pot
 
 cd ..
+
+# Copy the assets into the build directory.
 cp -r standard/assets build
 
 cd standard
+
 for lang in es fr; do
     echo "Building $lang..."
-    CODELIST_LANG=$lang python schema/utils/translate_codelists.py schema
-    CODELIST_LANG=$lang python schema/utils/translate_codelists.py docs/en/extensions
-    # Create a symlink for the current language, so we can reference the
-    # translated JSON schema from Sphinx directives
+
+    # Translate the schema and codelists for the language.
+    python schema/utils/translate_codelists.py schema $lang
+    python schema/utils/translate_codelists.py docs/en/extensions $lang
+
+    # Create a symlink for the current language, so we can reference the translated JSON Schema from Sphinx directives.
     rm ../build/current_lang
     ln -s ../build/$lang ../build/current_lang
+
+    # Build the language's documentation.
     sphinx-build -q -b dirhtml -D language="$lang" docs/en ../build/$lang
 done
 
-# Our deploy script doesn't like it if this still exists
+# The deploy script doesn't like it if this still exists.
 rm ../build/current_lang
