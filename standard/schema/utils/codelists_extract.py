e149668e7b6d50e55ac99ab6a69aa5790a99c186
James McKinney <james@slashpoundbang.com>
2018-01-31 18:40:35 -0500

    Skip extraction of Title in currency.csv

--- standard/schema/utils/codelists_extract.py
+++ standard/schema/utils/codelists_extract.py
@@ -3,18 +3,20 @@ Babel extractor used in setup.py
 """
 
 import csv
+import os.path
 from io import StringIO
 
 
 def extract(fileobj, keywords, comment_tags, options):
     """
-    Yields each CSV header, and the values in the Title and Description columns.
+    Yields each header, and the Title and Description values of a CSV file.
     """
     reader = csv.DictReader(StringIO(fileobj.read().decode()))
     for header in reader.fieldnames:
         yield 0, '', header, ''
 
-    for row_number, row in enumerate(reader, 1):
-        for key, value in row.items():
-            if key in ('Title', 'Description') and value:
-                yield row_number, '', value, [key]
+    if os.path.basename(fileobj.name) != 'currency.csv':
+        for row_number, row in enumerate(reader, 1):
+            for key, value in row.items():
+                if key in ('Title', 'Description') and value:
+                    yield row_number, '', value, [key]
