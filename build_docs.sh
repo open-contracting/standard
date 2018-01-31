40d286da605f2d608acf5094bda7c10132b16a44
James McKinney <james@slashpoundbang.com>
2018-01-31 18:33:26 -0500

    Have source build mirror translation builds more closely

--- build_docs.sh
+++ build_docs.sh
@@ -15,17 +15,17 @@ cd ..
 # Add translated JSON Schema files to the build directory.
 python standard/schema/utils/translate_schema.py en es fr
 
-# Create a symlink for the current language, so we can reference the source JSON Schema from Sphinx directives.
-rm -f build/current_lang
-ln -s en build/current_lang
-
 cd standard
 
-# Build the English documentation and message catalogs.
-sphinx-build -q -b dirhtml docs/en ../build/en
-sphinx-build -q -b gettext docs/en ../build/locale
+# Create a symlink for the current language, so we can reference the source JSON Schema from Sphinx directives.
+rm -f ../build/current_lang
+ln -s ../build/en ../build/current_lang
 
-# Use the Babel extractors.
+# Build the English documentation
+sphinx-build -q -b dirhtml docs/en ../build/en
+
+# Build the message catalogs and use the Babel extractors.
+sphinx-build -q -b gettext docs/en ../build/locale
 pybabel -q extract -F .babel_schema . -o ../build/locale/schema.pot
 pybabel -q extract -F .babel_codelists . -o ../build/locale/codelists.pot
 
@@ -39,7 +39,7 @@ cd standard
 for lang in es fr; do
     echo "Building $lang..."
 
-    # Translate the schema and codelists for the language.
+    # Translate the schema and codelist files for the language.
     python schema/utils/translate_codelists.py schema $lang
     python schema/utils/translate_codelists.py docs/en/extensions $lang
 
