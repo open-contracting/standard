40d07afa414c579cf3d4efb935a5eccaede8c165
James McKinney <james@slashpoundbang.com>
2018-02-02 00:09:33 -0500

    Update Babel configuration

--- build_pots.sh
+++ build_pots.sh
@@ -20,5 +20,5 @@ ln -s en build/current_lang
 # Build the pot files. These are the source files in .tx/config that can be pushed to Transifex.
 # See http://www.sphinx-doc.org/en/stable/builders.html#sphinx.builders.gettext.MessageCatalogBuilder
 sphinx-build -q -a -E -b gettext standard/docs/en build/locale
-pybabel -q extract -F standard/.babel_schema . -o build/locale/schema.pot
-pybabel -q extract -F standard/.babel_codelists . -o build/locale/codelists.pot
+pybabel -q extract -F .babel_schema . -o build/locale/schema.pot
+pybabel -q extract -F .babel_codelists . -o build/locale/codelists.pot
