a3d634ea5e9acd33567e1c54f93a4d881e02a948
James McKinney <james@slashpoundbang.com>
2018-02-02 00:24:37 -0500

    Add missing requirement

--- standard/schema/tests/test_validate_schema.py
+++ standard/schema/tests/test_validate_schema.py
@@ -5,10 +5,10 @@ duplicate keys.
 """
 
 import json
-from path import path
 from collections import UserDict
 
 from jsonschema import validate
+from path import path
 
 
 class RejectingDict(UserDict):
