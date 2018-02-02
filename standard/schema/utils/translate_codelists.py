7354758f2b8c59c41d2ab51a787d1e2f6a143224
James McKinney <james@slashpoundbang.com>
2018-02-02 15:23:17 -0500

    Revert part of 1320e2e7cb33cbde96848c68f1255ec22bc86949 as can't process multiple languages without overwriting

--- standard/schema/utils/translate_codelists.py
+++ standard/schema/utils/translate_codelists.py
@@ -4,7 +4,7 @@ relative `codelists_translated` directory.
 
 Usage:
 
-    python standard/schema/utils/translate_codelists.py directory localedir language [language ...]
+    python standard/schema/utils/translate_codelists.py directory localedir language
 
 `directory` is the path to the directory that contains the `codelists` directory containing codelist CSV files, and in
 which a `codelists_translated` directory will be created.
@@ -23,30 +23,29 @@ import sys
 
 directory = sys.argv[1]
 localedir = sys.argv[2]
-languages = sys.argv[3:]
+language = sys.argv[3]
 codelists_output_dir = os.path.join(directory, 'codelists_translated')
 
 if not os.path.exists(codelists_output_dir):
     os.makedirs(codelists_output_dir)
 
-for language in languages:
-    print('Translating codelists in {} to language {}'.format(directory, language))
+print('Translating codelists in {} to language {}'.format(directory, language))
 
-    translator = gettext.translation('codelists', localedir, languages=[language], fallback=language == 'en')
+translator = gettext.translation('codelists', localedir, languages=[language], fallback=language == 'en')
 
-    for file in glob.glob(os.path.join(directory, 'codelists', '*.csv')):
-        new_file = os.path.join(codelists_output_dir, os.path.basename(file))
-        with open(file) as r, open(new_file, 'w') as w:
-            reader = csv.DictReader(r)
-            fieldnames = [translator.gettext(fieldname) for fieldname in reader.fieldnames]
+for file in glob.glob(os.path.join(directory, 'codelists', '*.csv')):
+    new_file = os.path.join(codelists_output_dir, os.path.basename(file))
+    with open(file) as r, open(new_file, 'w') as w:
+        reader = csv.DictReader(r)
+        fieldnames = [translator.gettext(fieldname) for fieldname in reader.fieldnames]
 
-            writer = csv.DictWriter(w, fieldnames, lineterminator='\n')
-            writer.writeheader()
+        writer = csv.DictWriter(w, fieldnames, lineterminator='\n')
+        writer.writeheader()
 
-            for row in reader:
-                new_row = {}
-                for key, value in row.items():
-                    if key in ('Title', 'Description') and value:
-                        value = translator.gettext(value)
-                    new_row[translator.gettext(key)] = value
-                writer.writerow(new_row)
+        for row in reader:
+            new_row = {}
+            for key, value in row.items():
+                if key in ('Title', 'Description') and value:
+                    value = translator.gettext(value)
+                new_row[translator.gettext(key)] = value
+            writer.writerow(new_row)
