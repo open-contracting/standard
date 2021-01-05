27d1b888c7cb13efce79f721b2c43cb262bc6c5a
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-01-04 20:54:29 -0500

    js: Replace repetitive $(this) calls with $this const

--- docs/_static/json-example-format.js
+++ docs/_static/json-example-format.js
@@ -1,7 +1,8 @@
 /* global $, renderjson */
 
 $('.expandjson').each(function () {
-  const classList = $(this).attr('class').split(/\s+/)
+  const $this = $(this)
+  const classList = $this.attr('class').split(/\s+/)
   const expand = []
   let filename
   classList.forEach(function (item) {
@@ -12,26 +13,26 @@ $('.expandjson').each(function () {
       filename = item
     }
   })
-  const jsontext = $(this).text().trim()
+  const jsontext = $this.text().trim()
   let json = JSON.parse(jsontext)
   if (json.length) {
     json = json[0]
   }
-  $(this).html(renderjson.set_show_to_level(1).set_max_string_length(100).set_default_open(expand)(json))
-  if ($(this).siblings('.selection-container').length === 0) { // NEED TO FIX THE CODE HERE. MOVE THINGS INTO THE PARENT CLASS CORRECTLY!
+  $this.html(renderjson.set_show_to_level(1).set_max_string_length(100).set_default_open(expand)(json))
+  if ($this.siblings('.selection-container').length === 0) { // NEED TO FIX THE CODE HERE. MOVE THINGS INTO THE PARENT CLASS CORRECTLY!
     const id = Math.floor(5 * (Math.random() % 1))
-    $(this).wrap("<div class='selection-container'></div>")
-    $(this).parent().prepend(
+    $this.wrap("<div class='selection-container'></div>")
+    $this.parent().prepend(
       $("<select name='select-" + id + "'></select>")
         .change(function () {
-          $(this).siblings('.expandjson').hide()
-          $(this).siblings('.' + $(this).val()).show()
+          $this.siblings('.expandjson').hide()
+          $this.siblings('.' + $this.val()).show()
         }))
-    $(this).siblings('select').append($('<option></option>').attr('value', filename).text(filename.replace('file-', '')))
+    $this.siblings('select').append($('<option></option>').attr('value', filename).text(filename.replace('file-', '')))
   } else {
-    const container = $(this).siblings('.selection-container')
-    $(this).detach().appendTo(container)
-    $(this).siblings('select').append($('<option></option>').attr('value', filename).text(filename.replace('file-', '')))
-    $(this).hide()
+    const container = $this.siblings('.selection-container')
+    $this.detach().appendTo(container)
+    $this.siblings('select').append($('<option></option>').attr('value', filename).text(filename.replace('file-', '')))
+    $this.hide()
   }
 })
