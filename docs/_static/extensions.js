3fd2db12544acc9c44487bd736c4121e3c2f63b9
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-01-04 22:37:04 -0500

    js: Remove jQuery dependency from extensions.js

--- docs/_static/extensions.js
+++ docs/_static/extensions.js
@@ -1,44 +1,54 @@
-/* global $, location */
+/* global XMLHttpRequest, location */
 
 // FYI: extensionlist appears many times on /schema/reference
-if ($('.extension_list').length) {
+if (document.querySelector('.extension_list')) {
   const language = location.pathname.split('/')[2]
 
-  // A blank definition list for a community extension.
-  const template = '<dl><dt><a class="reference external" href=""></a></dt><dd></dd></dl>'
-
   // Append an empty list for community extensions.
-  $('.extension_list p.hide').after('<dl class="docutils hide community-list"></dl>').wrapInner('<small></small>')
+  document.querySelectorAll('.extension_list .hide').forEach(function (element) {
+    const dl = document.createElement('dl')
+    dl.className = 'simple community-list hide'
+    element.insertAdjacentElement('afterend', dl)
+    element.innerHTML = `<small>${element.innerHTML}</small>`
+  })
 
   // Get the community extensions to add to the documentation.
-  $.ajax({
-    url: 'https://raw.githubusercontent.com/open-contracting/extension_registry/master/build/extensions.json',
-    crossDomain: true,
-    dataType: 'json'
-  }).done(function (data) {
-    const communityExtensions = $.grep(data.extensions, function (extension) {
-      return !extension.core
-    })
+  const request = new XMLHttpRequest()
+  request.open('GET', 'https://raw.githubusercontent.com/open-contracting/extension_registry/master/build/extensions.json')
+  request.responseType = 'json'
 
-    // Add community extensions.
-    communityExtensions.forEach(function (extension) {
-      const extensionName = extension.name[language] || extension.name.en
-      const extensionDescription = extension.description[language] || extension.description.en
-      const extensionListId = '#extensionlist-' + extension.category
-      const $item = $(template)
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      // Add community extensions.
+      request.response.extensions.forEach(function (extension) {
+        if (!extension.core) {
+          const div = document.getElementById('extensionlist-' + extension.category)
+          if (div) {
+            div.querySelector('.community-list').insertAdjacentHTML('beforeend', `
+              <dt>
+                <a class="reference external" href="${extension.documentation_url}">
+                  ${extension.name[language] || extension.name.en}
+                </a>
+              </dt>
+              <dd>
+                ${extension.description[language] || extension.description.en}
+              </dd>
+            `)
+            div.querySelectorAll('.hide').forEach(function (element) {
+              element.classList.remove('hide')
+            })
+          }
+        }
+      })
 
-      $item.find('a').attr('href', extension.documentation_url).text(extensionName)
-      $item.find('dd').text(extensionDescription)
-      $(extensionListId).find('.community-list').append($item.html()).removeClass('hide')
-      $(extensionListId).find('p.hide').removeClass('hide')
-    })
+      // Remove empty extension lists.
+      document.querySelectorAll('.extension_list').forEach(function (element) {
+        if (!element.querySelector('a')) {
+          element.parentNode.removeChild(element)
+        }
+      })
+    }
+  }
 
-    // Remove empty extension lists.
-    $('.extension_list').each(function () {
-      const $this = $(this)
-      if (!$this.find('a').attr('href')) {
-        $this.remove()
-      }
-    })
-  })
+  request.send()
 }
