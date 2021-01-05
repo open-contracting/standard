3e3d452788b141901f2f3df0af540615cc588858
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-01-05 00:36:41 -0500

    js: Remove jQuery dependency from json-example-format.js. Fix TODO. Hide select if only one option.

--- docs/_static/json-example-format.js
+++ docs/_static/json-example-format.js
@@ -1,38 +1,55 @@
 /* global $, renderjson */
 
-$('.expandjson').each(function () {
-  const $this = $(this)
-  const classList = $this.attr('class').split(/\s+/)
-  const expand = []
-  let filename
-  classList.forEach(function (item) {
-    if (item.indexOf('expand') === 0) {
-      expand.push(item.replace('expand-', ''))
+let groupIndex = 0;
+
+document.querySelectorAll('.expandjson').forEach(function (element) {
+  const defaultOpen = []
+  let fileClassName
+  element.classList.forEach(function (className) {
+    if (className.substring(0, 7) === 'expand-' && className.length > 7) {
+      defaultOpen.push(className.substring(7))
     }
-    if (item.indexOf('file') === 0) {
-      filename = item
+    if (className.substring(0, 5) === 'file-') {
+      fileClassName = className
     }
   })
-  const jsontext = $this.text().trim()
-  let json = JSON.parse(jsontext)
-  if (json.length) {
-    json = json[0]
+
+  let data = JSON.parse(element.textContent)
+  // If the jsoninclude directive indexed to a JSON array (a common mistake), only display the first entry.
+  if (data.length) {
+    data = data[0]
   }
-  $this.html(renderjson.set_show_to_level(1).set_max_string_length(100).set_default_open(expand)(json))
-  if ($this.siblings('.selection-container').length === 0) { // NEED TO FIX THE CODE HERE. MOVE THINGS INTO THE PARENT CLASS CORRECTLY!
-    const id = Math.floor(5 * (Math.random() % 1))
-    $this.wrap("<div class='selection-container'></div>")
-    $this.parent().prepend(
-      $("<select name='select-" + id + "'></select>")
-        .change(function () {
-          $this.siblings('.expandjson').hide()
-          $this.siblings('.' + $this.val()).show()
-        }))
-    $this.siblings('select').append($('<option></option>').attr('value', filename).text(filename.replace('file-', '')))
+
+  const replacement = renderjson.set_show_to_level(1).set_max_string_length(100).set_default_open(defaultOpen)(data)
+  // element.firstElementChild.replaceWith(replacement) // https://caniuse.com/mdn-api_parentnode_firstelementchild
+  element.replaceChild(replacement, element.querySelector('.highlight-json'))
+
+  const container = element.previousElementSibling
+  let select
+  if (container && container.classList.contains('selection-container')) {
+    element.style.display = 'none'
+    container.appendChild(element)
+    select = container.querySelector('select')
+    select.style.display = ''
   } else {
-    const container = $this.siblings('.selection-container')
-    $this.detach().appendTo(container)
-    $this.siblings('select').append($('<option></option>').attr('value', filename).text(filename.replace('file-', '')))
-    $this.hide()
+    const div = document.createElement('div')
+    element.insertAdjacentElement('beforebegin', div)
+    div.className = 'selection-container'
+    div.appendChild(element)
+
+    select = document.createElement('select')
+    div.insertAdjacentElement('afterbegin', select)
+    select.style.display = 'none'
+    select.addEventListener('change', function () {
+      div.querySelectorAll('.expandjson').forEach(function (sibling) {
+        sibling.style.display = 'none'
+      })
+      div.querySelector(`.${select.value}`).style.display = ''
+    })
   }
+
+  const option = document.createElement('option')
+  option.value = fileClassName
+  option.textContent = fileClassName.replace('file-', '')
+  select.append(option)
 })
