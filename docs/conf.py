14c7fe347092b563dca41133c9bd978946b07c52
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-02-13 23:58:04 -0500

    conf: Remove extension_versions config value

--- docs/conf.py
+++ docs/conf.py
@@ -105,7 +105,6 @@ extension_versions = {
 
 
 def setup(app):
-    app.add_config_value('extension_versions', extension_versions, True)
     app.add_config_value('recommonmark_config', {
         'auto_toc_tree_section': 'Contents',
         'enable_eval_rst': True
