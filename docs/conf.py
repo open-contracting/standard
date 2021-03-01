0071cdb4d7ad4d72dc924ec9539a90d8970b615b
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-03-01 16:58:04 -0500

    conf: Exclude docson to avoid sphinx-build -b gettext warnings

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_static/docson/*.md', '_static/docson/integration/*.md']
 
 
 # -- Options for HTML output -------------------------------------------------
