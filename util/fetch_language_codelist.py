aca29923c8749cd573dc0433d44adfeec9e84564
Duncan Dewhurst <duncan.dewhurst@opendataservices.coop>
2021-06-22 12:00:48 +1200

    Remove repetitions, use comma as separator

--- util/fetch_language_codelist.py
+++ util/fetch_language_codelist.py
@@ -4,6 +4,7 @@ Updates the language codelist from the Library of Congress ISO639-1 TSV file.
 
 import csv
 import os
+import re
 import requests
 
 from helper import schema_dir
@@ -13,13 +14,15 @@ if __name__ == '__main__':
 
     codes = {}
 
+    # https://www.iso.org/iso-639-language-codes.html links to loc.gov.
     response = requests.get('http://id.loc.gov/vocabulary/iso639-1.tsv')
     response.raise_for_status()
     content = response.content.decode('utf-8')
     csvreader = csv.DictReader(content.splitlines(), delimiter='\t')
     for row in csvreader:
         code = row['code']
-        title = row['Label (English)']
+        titles = re.split(r' *\| *', row['Label (English)'])
+        title = ', '.join({' '.join(reversed(title.split(', '))): None for title in titles})
         codes[code] = title
 
     with open(os.path.join(schema_dir, 'codelists', 'language.csv'), 'w') as fp:
