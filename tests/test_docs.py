a7b62d5a9afccb7abec4bd22ba6bb2b24e123c3d
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-04-06 18:16:39 -0400

    build: Upgrade to latest profile template

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,4 +1,5 @@
 import pytest
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
 from . import languages
@@ -7,8 +8,8 @@ from . import languages
 @pytest.mark.parametrize('lang', list(languages))
 def test_examples(browser, server, lang):
     browser.get(f'{server}{lang}/guidance/build/merging')
-    examples = browser.find_element_by_id('updates-and-deletions')
-    select = Select(examples.find_element_by_tag_name('select'))
+    examples = browser.find_element(By.ID, 'updates-and-deletions')
+    select = Select(examples.find_element(By.TAG_NAME, 'select'))
 
     assert '"date": "2016-01-01T09:30:00Z"' in examples.text
 
@@ -17,4 +18,4 @@ def test_examples(browser, server, lang):
     assert '"date": "2016-01-01T09:30:00Z"' not in examples.text
 
     # test collapse expand
-    # xs = examples.find_element_by_link_text('⊖')
+    # xs = examples.find_element(By.LINK_TEXT, '⊖')
