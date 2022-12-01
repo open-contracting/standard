19c6bc2bc17beef0ab8b5f7c2e64640e6174cc22
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-12-01 16:42:46 -0500

    build: Remove sphinx-panels (obsoleted by sphinx-design)

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,6 @@ release = '1.1.5'
 extensions = [
     'myst_parser',
     'sphinx.ext.ifconfig',
-    'sphinx_panels',
     'sphinxcontrib.jsonschema',
     'sphinxcontrib.opencontracting',
     'sphinxcontrib.opendataservices',
