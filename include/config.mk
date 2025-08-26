7b276c4f493242776c1b175152a574a0de1eba77
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-08-26 10:13:29 -0400

    build: Remove Transifex and add Crowdin configuration

--- include/config.mk
+++ include/config.mk
@@ -19,8 +19,6 @@ DIST_FILES=
 POT_DIR=$(BUILD_DIR)/locale
 # The prefix, if any, to the schema and codelists domains.
 DOMAIN_PREFIX=
-# The Transifex project name.
-TRANSIFEX_PROJECT=open-contracting-standard-1-1
 # Any additional extract targets.
 EXTRACT_TARGETS=
 # Extra arguments for sphinx-autobuild.
